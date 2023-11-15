be3e6f371f888a77ae969db3ffd52e518266bab1
Cryszzz <crystalzou000829@gmail.com>
2023-11-14 21:34:37 -0500

    change

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import styles from './HomePage.module.css';
+import ShadowMapping from '../sample/shadowMapping/main';
+
+const HomePage: React.FunctionComponent = () => {
+  return <ShadowMapping/>;
+};
+
+export default HomePage;
--- src/shaders/raytracer_kernel.wgsl
+++
@@ -1,6 +0,0 @@
-
-
-@compute @workgroup_size(8,8,1)
-fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
-    
-}
