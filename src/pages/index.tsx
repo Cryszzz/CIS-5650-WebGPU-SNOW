6abce256c67907f5376a72c1bd1817f594950e96
Cryszzz <69176305+Cryszzz@users.noreply.github.com>
2023-12-09 04:38:43 +0000

    Deploying to gh-pages from  @ b741f46bd4aec4054316463264218165fdc216fa 🚀

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './HomePage.module.css';
-import ShadowMapping from '../sample/shadowMapping/main';
-
-const HomePage: React.FunctionComponent = () => {
-  return <ShadowMapping/>;
-};
-
-export default HomePage;
